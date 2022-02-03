e69104c1c478edd93e862c96d63e4ce905843226
DhruvK1999 <dkanthaliya1999@gmail.com>
2022-02-03 11:53:50 +0530

    configurational changes

---
+++ indicators.py
@@ -0,0 +1,36 @@
+# method for calculating the RSI
+# to-do get this in a different file and make a package
+
+#indicators.py
+
+def rsi(df, periods = 14, ema = True):
+    """
+    Returns a pd.Series with the relative strength index.
+    """
+    close_delta = df['Close'].diff()
+
+    # Make two series: one for lower closes and one for higher closes
+    up = close_delta.clip(lower=0)
+    down = -1 * close_delta.clip(upper=0)
+    
+    if ema == True:
+	    # Use exponential moving average
+        ma_up = up.ewm(com = periods - 1, adjust=True, min_periods = periods).mean()
+        ma_down = down.ewm(com = periods - 1, adjust=True, min_periods = periods).mean()
+    else:
+        # Use simple moving average
+        ma_up = up.rolling(window = periods, adjust=False).mean()
+        ma_down = down.rolling(window = periods, adjust=False).mean()
+        
+    rsi = ma_up / ma_down
+    rsi = 100 - (100/(1 + rsi))
+    df["rsi"]=rsi
+    return df
+    
+def sma(df,period):
+    df[f'sma{period}'] = df['Close'].rolling(period).mean()
+    return df
+
+
+    
+    
